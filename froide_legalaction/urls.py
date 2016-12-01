bb70a1a3dc670624864974c555b2bf700728f480
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2016-12-01 17:10:55 +0100

    Make thanks url translatable

--- froide_legalaction/urls.py
+++ froide_legalaction/urls.py
@@ -7,6 +7,6 @@ from .views import request_form_page, thanks_page
 
 urlpatterns = [
     url(r'^$', xframe_options_exempt(request_form_page), name='legalaction-index'),
-    url(r'^thanks/$', xframe_options_exempt(thanks_page), name='legalaction-thanks'),
+    url(_(r'^thanks/$'), xframe_options_exempt(thanks_page), name='legalaction-thanks'),
     url(_(r'^request/(?P<pk>\d+)/$'), request_form_page, name='legalaction-request_form'),
 ]
