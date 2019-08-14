2840c96336bdf03690c1559dbe823a776337bb73
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-08-14 14:37:34 +0200

    Translate URLs with context

--- froide_legalaction/urls.py
+++ froide_legalaction/urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls import url
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import pgettext_lazy
 from django.views.decorators.clickjacking import xframe_options_exempt
 
 
 from .views import request_form_page, thanks_page
 
 urlpatterns = [
-    url(r'^$', xframe_options_exempt(request_form_page), name='legalaction-index'),
-    url(_(r'^thanks/$'), xframe_options_exempt(thanks_page), name='legalaction-thanks'),
-    url(_(r'^request/(?P<pk>\d+)/$'), request_form_page, name='legalaction-request_form'),
+    url(r'^$', xframe_options_exempt(request_form_page),
+        name='legalaction-index'),
+    url(pgettext_lazy('url part', r'^thanks/$'),
+        xframe_options_exempt(thanks_page), name='legalaction-thanks'),
+    url(pgettext_lazy('url part', r'^request/(?P<pk>\d+)/$'),
+        request_form_page, name='legalaction-request_form'),
 ]
