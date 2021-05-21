3d5154e37378726f3ed4a88a20875417030448b2
Magdalena Noffke <magdalena.noffke@googlemail.com>
2021-05-21 17:47:58 +0200

    Add klageautomat views

--- froide_legalaction/urls.py
+++ froide_legalaction/urls.py
@@ -3,7 +3,8 @@ from django.utils.translation import pgettext_lazy
 from django.views.decorators.clickjacking import xframe_options_exempt
 
 
-from .views import request_form_page, thanks_page
+from .views import (request_form_page, thanks_page,
+                    klageautomat, KlageAutomatWizard)
 
 urlpatterns = [
     url(r'^$', xframe_options_exempt(request_form_page),
@@ -12,4 +13,8 @@ urlpatterns = [
         xframe_options_exempt(thanks_page), name='legalaction-thanks'),
     url(pgettext_lazy('url part', r'^request/(?P<pk>\d+)/$'),
         request_form_page, name='legalaction-request_form'),
+    url(pgettext_lazy('url part', r'^klageautomat/$'),
+        klageautomat, name='klageautomat-index'),
+    url(pgettext_lazy('url part', r'^klageautomat/request/(?P<pk>\d+)/$'),
+        KlageAutomatWizard.as_view(), name='klageautomat-form_wizard'),
 ]
--- froide_legalaction/views.py
+++ froide_legalaction/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, get_object_or_404, Http404, redirect
 from django.urls import reverse
 from django.contrib import messages
+from django.contrib.admin.views.decorators import staff_member_required
 from django.utils.translation import gettext_lazy as _
+from django.utils.decorators import method_decorator
+
+from legal_advice_builder.views import FormWizardView
+from legal_advice_builder.models import LawCase
 
 from froide.foirequest.models import FoiRequest
 from froide.foirequest.auth import can_write_foirequest
@@ -65,3 +70,69 @@ def thanks_page(request):
     return render(request, 'froide_legalaction/thanks.html', {
         'base_template': base_template
     })
+
+
+@staff_member_required
+def klageautomat(request):
+    return render(request, 'legal_advice_builder/foirequest_list.html', {})
+
+
+@method_decorator(staff_member_required, name='dispatch')
+class KlageAutomatWizard(FormWizardView):
+
+    def get_lawcase(self):
+        return LawCase.objects.all().first()
+
+    def get_foirequest(self):
+        pk = self.kwargs.get('pk')
+        return FoiRequest.objects.get(pk=pk)
+
+    def get_messages_as_options(self, foirequest):
+        messages = {}
+        for message in foirequest.messages:
+            key = 'message_{}'.format(message.id)
+            messages[key] = message
+        return messages
+
+    def get_initial_dict(self):
+        foi_request = self.get_foirequest()
+        return {
+            'pruefen': {
+                'antragstellung': {
+                    'initial': foi_request.first_message.date().strftime(
+                        "%Y-%m-%d")
+                }
+            },
+            'person': {
+                'behoerde_name': {
+                    'initial': foi_request.public_body.name
+                },
+                'behoerde_adresse': {
+                    'initial': foi_request.public_body.address
+                },
+                'anfrage_gesetz': {
+                    'initial': [foi_request.law.law_type]
+                },
+                'anfrage_text': {
+                    'initial': foi_request.messages[0].plaintext
+                },
+                'anfrage_datum': {
+                    'initial': foi_request.first_message.date()
+                },
+                'anfrage_frist': {
+                    'initial': foi_request.due_date.date().strftime(
+                        "%Y-%m-%d"),
+                },
+                'name': {
+                    'initial': '{} {}'.format(
+                        foi_request.user.first_name,
+                        foi_request.user.last_name)
+                },
+                'adresse': {
+                    'initial': foi_request.user.address
+                },
+                'anhaenge': {
+                    'options': self.get_messages_as_options(foi_request)
+                }
+            }
+        }
