d2635b35f8f0c92be1c191af08f9f30e94d84c04
Magdalena Noffke <magdalena.noffke@googlemail.com>
2021-05-21 18:57:29 +0200

    Add foirequest to context

--- froide_legalaction/views.py
+++ froide_legalaction/views.py
@@ -80,6 +80,13 @@ def klageautomat(request):
 @method_decorator(staff_member_required, name='dispatch')
 class KlageAutomatWizard(FormWizardView):
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'foi_request': self.get_foirequest()
+        })
+        return context
+
     def get_lawcase(self):
         return LawCase.objects.all().first()
 
