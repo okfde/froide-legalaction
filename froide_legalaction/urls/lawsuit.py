6a536011b79467c95e27faf1609887f34dbe8cc6
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2024-05-29 12:13:09 +0200

    Add ical feed for lawsuit court hearings

--- froide_legalaction/urls/lawsuit.py
+++ froide_legalaction/urls/lawsuit.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.utils.translation import pgettext_lazy
 
-from ..views import request_form_page, thanks_page
+from ..views import lawsuit_event_calendar, request_form_page, thanks_page
 
 urlpatterns = [
     path(
@@ -13,6 +13,11 @@ urlpatterns = [
         pgettext_lazy("url part", "thanks/"),
         thanks_page,
         name="legalaction-thanks",
+    ),
+    path(
+        pgettext_lazy("url part", "calendar/ical/"),
+        lawsuit_event_calendar,
+        name="legalaction-lawsuit_calendar",
     ),
     path(
         pgettext_lazy("url part", "request/<int:pk>/"),
